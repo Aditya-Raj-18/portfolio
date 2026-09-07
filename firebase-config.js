10395016f1bd0278143ca81dd50ea5d344573a91
Aditya Raj <adityat9122@gmail.com>
2026-09-07 20:06:24 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBl0-Hod2nj3EAbPUTX60SESfdnN_b_8sQ",
+  authDomain: "aditya-portfolio-eacd9.firebaseapp.com",
+  projectId: "aditya-portfolio-eacd9",
+  storageBucket: "aditya-portfolio-eacd9.firebasestorage.app",
+  messagingSenderId: "821167579679",
+  appId: "1:821167579679:web:c67d1ba6567d5f7c236b2a"
+};
